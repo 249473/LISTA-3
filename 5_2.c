e162263eb9fbf9655cde78c13dd1b2b11842ebc8
249473 <62726207+249473@users.noreply.github.com>
2020-11-26 11:55:41 +0100

    Update 5_2.c

--- 5_2.c
+++ 5_2.c
@@ -3,40 +3,40 @@
 #include <fcntl.h>
 #define Buf_rozmiar 256
 int main(int argc, char *argv[]) {
-
+	
 	pid_t pid;
 	int fd[2], in_fd, n;
 	char buf [Buf_rozmiar];
 
-	if(pipe(fd)<0)
+	if(pipe(fd)<0) //sprawdzanie czy pipe stworzyl sie poprawnie
         {
         fprintf(stderr, "eror\n");
         }
 
-	pid = fork();
+	pid = fork();//tworzenie syna
 
-	if(pid==0)
+	if(pid==0)//syn
 		{
 		close(fd[1]);
 		close(0);
 		dup(fd[0]);
 		close(fd[0]);
-		execlp("display","display","-",NULL);
+		execlp("display","display","-",NULL); //wyswietlanie obrazka
             //close(fd[0]);
 		}
 
-	else if(pid>0)
+	else if(pid>0)//rodzic
 	{
 		close(fd[0]);
 		//scanf("%s",fname);
-		if((in_fd=open(argv[1],O_RDONLY))<0)
+		if((in_fd=open(argv[1],O_RDONLY))<0)//obsluga bledu wyswietlania
 			{
 			fprintf(stderr,"eror\n");
 			return 2;
 			}
 
-	while ((n= read(in_fd, &buf,Buf_rozmiar))>0){
-		if(write(fd[1],&buf,n)<0){
+	while ((n= read(in_fd, &buf,Buf_rozmiar))>0){//wczytywanie danych
+		if(write(fd[1],&buf,n)<0){//obsluga bledy wczytywania
 			fprintf(stderr,"eror\n");
 			return 3;
 			}
